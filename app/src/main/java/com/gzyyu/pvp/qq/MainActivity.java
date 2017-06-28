cce832510f06502cd2dab8bf8bd30fff3ef99c74
GZYYU <gzyyu123@163.com>
2017-06-28 20:06:18 +0800

    Two

--- app/src/main/java/com/gzyyu/pvp/qq/MainActivity.java
+++ app/src/main/java/com/gzyyu/pvp/qq/MainActivity.java
@@ -24,11 +24,6 @@ public class MainActivity extends AppCompatActivity {
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         WebView webView = (WebView) findViewById(R.id.web_view);
         webView.requestFocusFromTouch();
-        /*切记要添加这一行代码，因为Android中的WebView默认是不响应JavaScript控件的，
-        如果不加下面这一行，就会出现一个很奇怪的问题，网上也有很多人都在问，
-        为什么加载的网页链接可以点击并跳转，但是按钮点了却没反应。
-        因为很多网页的控件是通过js编写的，所以要添加下面一行代码以便响应JS控件。*/
-        webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         WebSettings webSettings = webView.getSettings();
         webSettings.setUseWideViewPort(true);//设置此属性，可任意比例缩放
@@ -45,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this, "页面正在加载,请稍后......",Toast.LENGTH_SHORT).show();
             }
-
             //网页加载完成时调用，隐藏加载提示旋转进度条
             @Override
             public void onPageFinished(WebView view, String url) {
@@ -53,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.GONE);
                 Toast.makeText(MainActivity.this, "页面加载完成.",Toast.LENGTH_SHORT).show();
             }
-
             //网页加载失败时调用，隐藏加载提示旋转进度条
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
@@ -62,7 +55,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "网页加载失败,请检查网络",Toast.LENGTH_SHORT).show();
             }
         });
-
     }
-
 }
